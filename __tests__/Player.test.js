252009ddc7e07d5eb9fdb5d091080427d55566ff
Marvin Ren <marvin.ren@yale.edu>
2021-10-11 15:25:58 -0400

    added method to Object

---
+++ __tests__/Player.test.js
@@ -0,0 +1,33 @@
+const Potion = require('../lib/Potion');
+jest.mock('../lib/Potion');
+const Player = require('../lib/Player');
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+});
+
+test("get player's status as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStatus()).toHaveProperty('potions');
+    expect(player.getStatus()).toHaveProperty('health');
+    expect(player.getStatus()).toHaveProperty('strength');
+    expect(player.getStatus()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+    
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
