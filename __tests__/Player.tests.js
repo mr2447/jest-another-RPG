18282739d0fadbbb81f7f91e2af689cac12e1fdc
Marvin Ren <marvin.ren@yale.edu>
2021-10-11 12:59:30 -0400

    questions

--- __tests__/Player.tests.js
+++ __tests__/Player.tests.js
@@ -1,3 +1,6 @@
+const Potion = require('../lib/Potion');
+jest.mock('../lib/Potion');
+console.log(new Potion());
 const Player = require('../lib/Player');
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -6,4 +9,7 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContainting([expect.any(Object)])
+    );
 });
