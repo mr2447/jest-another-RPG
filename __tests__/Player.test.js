d420b87fb232a8c03ad20ab368a550e74ccbf36b
Marvin Ren <marvin.ren@yale.edu>
2021-10-13 12:08:54 -0400

    error

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -50,11 +50,11 @@ test ("subtracts from player's health", () => {
     const player = new Player('Dave');
     const oldHealth = player.health;
 
-    player.reducedHealth(5);
+    player.reduceHealth(5);
 
     expect(player.health).toBe(oldHealth - 5);
 
-    player.reducedHealth(999999)
+    player.reduceHealth(999999)
 
     expect(player.health).toBe(0);
 });
